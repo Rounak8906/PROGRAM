58eb5af6d6714729a0b38edaeee954d92f5d9a8c
Rounak8906 <rounaksingh8906@gmail.com>
2026-02-01 16:36:09 +0530

    Added oops.py File

---
+++ oops.py
@@ -0,0 +1,14 @@
+class suthar:
+    def __init__(self,name,address):
+        self.name = name
+        self.address = address
+    def jaisalmer(self):
+        print(f"i am pyhton king {self.name}")
+
+
+
+junjar_u = suthar("Junjar","Hosur")
+junjar_u.subject = "python"
+print(junjar_u.name)
+print(junjar_u.address)
+junjar_u.jaisalmer()
